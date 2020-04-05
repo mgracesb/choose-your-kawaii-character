45f3ffcb4a5bfcfbd40d81ca590b20fdf54e0ae6
mgracesb <mercysbarcel@gmail.com>
2020-04-05 23:47:29 +0200

    add button

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import "./styles/App.css";
 import Preview from "./Preview";
+import Button from "./components/Button";
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
         <Preview />
+        <Button />
       </div>
     );
   }
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,3 +1,14 @@
 import React from "react";
 
+class Button extends React.Component {
+  handleChange = (evt) => preventDefault(evt);
+
+  render() {
+    return (
+      <button className="button" onClick={handleChange}>
+        Change me!
+      </button>
+    );
+  }
+}
 export default Button;
