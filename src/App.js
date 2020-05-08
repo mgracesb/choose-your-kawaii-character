28eb620cf3a30bcf93f0937c84d8059f9c9b2b2b
mgracesb <mercysbarcel@gmail.com>
2020-05-08 22:34:03 +0200

    add styles for tablet and pc

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./styles/App.css";
 import Character from "./components/Character";
 import { Item1 } from "./components/Items";
 // import Item2 from "./components/Items";
+import Header from "./components/Header";
 import Bg from "./components/Bg";
 import Preview from "./components/Preview";
 
@@ -37,6 +38,7 @@ class App extends React.Component {
     const { bg, character, item1 } = this.state;
     return (
       <div className="App">
+        <Header />
         <Preview bg={bg} char={character} item1={item1} />
         <Bg bgHandler={this.bgHandler} />
         <Character charHandler={this.charHandler} />
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <h1>Your Spirit Kawaii</h1>
+    </header>
+  );
+};
+
+export default Header;
