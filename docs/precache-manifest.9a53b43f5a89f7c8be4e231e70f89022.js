35032c280fd34c8df314184dc427cf7887ea8172
mgracesb <mercysbarcel@gmail.com>
2020-05-22 14:15:50 +0200

    update production files

---
+++ docs/precache-manifest.9a53b43f5a89f7c8be4e231e70f89022.js
@@ -0,0 +1,226 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "131ae4d656ea34808cd6ae91543bcc99",
+    "url": "/index.html"
+  },
+  {
+    "revision": "151f9a29e22ba6b47aac",
+    "url": "/static/css/main.0fb58c7d.chunk.css"
+  },
+  {
+    "revision": "dc37fa7be82d5ae7f908",
+    "url": "/static/js/2.d2c94ec7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d2c94ec7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "151f9a29e22ba6b47aac",
+    "url": "/static/js/main.140f0645.chunk.js"
+  },
+  {
+    "revision": "cd19f7033169da15c23e",
+    "url": "/static/js/runtime-main.306bd78a.js"
+  },
+  {
+    "revision": "ddc0b98af481ac3937efcc54f881d4dc",
+    "url": "/static/media/alien.ddc0b98a.svg"
+  },
+  {
+    "revision": "e559842ed951f20ed513d9b1a4929a45",
+    "url": "/static/media/astronomy.e559842e.png"
+  },
+  {
+    "revision": "41df8897eb4e1f85767126bc488178d6",
+    "url": "/static/media/beach.41df8897.jpg"
+  },
+  {
+    "revision": "3c8cc0eb47cfa7e31eba3bf6962a1e61",
+    "url": "/static/media/circles.3c8cc0eb.jpg"
+  },
+  {
+    "revision": "b1f90c259935386d9c96fb5096066190",
+    "url": "/static/media/circuit.b1f90c25.jpg"
+  },
+  {
+    "revision": "a9bd2a5af23d1f401113bb8040a521e0",
+    "url": "/static/media/death.a9bd2a5a.svg"
+  },
+  {
+    "revision": "c6850d2891849458d7220ab1819c4381",
+    "url": "/static/media/dog.c6850d28.svg"
+  },
+  {
+    "revision": "92601fada8897a8be9b72482acf92e00",
+    "url": "/static/media/eighties.92601fad.png"
+  },
+  {
+    "revision": "2a811774c706b626cf20461fef6cc5d3",
+    "url": "/static/media/fish.2a811774.svg"
+  },
+  {
+    "revision": "547b30eef8b8c233d499c9bae77d3e12",
+    "url": "/static/media/hamster.547b30ee.svg"
+  },
+  {
+    "revision": "f04ec767f7a5d8875b0acb863c795a08",
+    "url": "/static/media/item10.f04ec767.svg"
+  },
+  {
+    "revision": "1dbb9d7bfeb21724fc154d303d2516d5",
+    "url": "/static/media/item11.1dbb9d7b.svg"
+  },
+  {
+    "revision": "fcf59806679ad0dc150d8c9116f9503a",
+    "url": "/static/media/item12.fcf59806.svg"
+  },
+  {
+    "revision": "af259e0f3b65b9bf51b098b3afad1bb2",
+    "url": "/static/media/item13.af259e0f.svg"
+  },
+  {
+    "revision": "7a1e101cd48c6d19cdeaa8d2a6367eb5",
+    "url": "/static/media/item14.7a1e101c.svg"
+  },
+  {
+    "revision": "e117e2f167350a43185640dd9fd76d2f",
+    "url": "/static/media/item15.e117e2f1.svg"
+  },
+  {
+    "revision": "c0e85a135aee51e5dd5c3f25f9c3882f",
+    "url": "/static/media/item16.c0e85a13.svg"
+  },
+  {
+    "revision": "ad27a6bcebe16f51c794312fd72d0fa5",
+    "url": "/static/media/item17.ad27a6bc.svg"
+  },
+  {
+    "revision": "23eeea342e3b6ed934144d1e9abf02ab",
+    "url": "/static/media/item18.23eeea34.svg"
+  },
+  {
+    "revision": "74fb8c7a06ea3997a5a8c1ea4a083f70",
+    "url": "/static/media/item19.74fb8c7a.svg"
+  },
+  {
+    "revision": "316f49864b8ebfec08f082f088ccd92d",
+    "url": "/static/media/item20.316f4986.svg"
+  },
+  {
+    "revision": "db45d66e94fe09cac29b4ebcc1b4f336",
+    "url": "/static/media/item22.db45d66e.svg"
+  },
+  {
+    "revision": "f76d16de49c72b7d2cb99eccc949aa64",
+    "url": "/static/media/item23.f76d16de.svg"
+  },
+  {
+    "revision": "2ee515457c1b5fec728d4812caf68132",
+    "url": "/static/media/item25.2ee51545.svg"
+  },
+  {
+    "revision": "3a622db62fd61a010639aa3e4776edc9",
+    "url": "/static/media/item26.3a622db6.svg"
+  },
+  {
+    "revision": "37f3e143c1fcc6a7a8ad730fd9092c06",
+    "url": "/static/media/item27.37f3e143.svg"
+  },
+  {
+    "revision": "2cb0ef9f9159f9d6dcac5b048b6fe3d5",
+    "url": "/static/media/item28.2cb0ef9f.svg"
+  },
+  {
+    "revision": "5a725baf61f8fae017372cb091f3e498",
+    "url": "/static/media/item29.5a725baf.svg"
+  },
+  {
+    "revision": "02b1111619d492ae9c4e4e984feb91dc",
+    "url": "/static/media/item30.02b11116.svg"
+  },
+  {
+    "revision": "83ff420f43a2fa2e977ee50ead4b2612",
+    "url": "/static/media/item31.83ff420f.svg"
+  },
+  {
+    "revision": "c36b09a919a957375bd79256d6f1c99e",
+    "url": "/static/media/item32.c36b09a9.svg"
+  },
+  {
+    "revision": "e31640e93e79b115faad8cda15ccf0c0",
+    "url": "/static/media/item33.e31640e9.svg"
+  },
+  {
+    "revision": "b7f737956e3e28f13bb86a420e6370b5",
+    "url": "/static/media/item34.b7f73795.svg"
+  },
+  {
+    "revision": "66aa262a24209f629888e33c2aeebf26",
+    "url": "/static/media/item35.66aa262a.svg"
+  },
+  {
+    "revision": "93a6fafc78e85855c94f336600150139",
+    "url": "/static/media/item36.93a6fafc.svg"
+  },
+  {
+    "revision": "a0f6b044658de40a96b8308f969431e7",
+    "url": "/static/media/item38.a0f6b044.svg"
+  },
+  {
+    "revision": "a8eff9009d20ce99cd528daace6cdbaf",
+    "url": "/static/media/item39.a8eff900.svg"
+  },
+  {
+    "revision": "8cab41de7e5bee2ba7d185e1ea360d2f",
+    "url": "/static/media/item40.8cab41de.svg"
+  },
+  {
+    "revision": "5b0bba1837ea176c4dde898deee88d3e",
+    "url": "/static/media/item41.5b0bba18.svg"
+  },
+  {
+    "revision": "0de6a0d721b25254dae3e18b96395322",
+    "url": "/static/media/item42.0de6a0d7.svg"
+  },
+  {
+    "revision": "2ce708a4620b885464cd9b41edcb8f40",
+    "url": "/static/media/koala.2ce708a4.svg"
+  },
+  {
+    "revision": "2d4d4018c1f1afcc9e8358d0bf433656",
+    "url": "/static/media/lion.2d4d4018.svg"
+  },
+  {
+    "revision": "f3bbba9100695681a8312b3232a245cb",
+    "url": "/static/media/mermaid.f3bbba91.jpg"
+  },
+  {
+    "revision": "8db404044c647b618568796cd066e1d5",
+    "url": "/static/media/monkey.8db40404.svg"
+  },
+  {
+    "revision": "fbd97ca447ec91e578291a94064519a7",
+    "url": "/static/media/panda.fbd97ca4.svg"
+  },
+  {
+    "revision": "3bf38ed585da1e0296709dd3d6675d37",
+    "url": "/static/media/pink.3bf38ed5.jpg"
+  },
+  {
+    "revision": "a91f07f6c24433ca52e62d197a4f582f",
+    "url": "/static/media/rainbow.a91f07f6.jpg"
+  },
+  {
+    "revision": "a2c5d884f5fe0979db3cf8a8eaf52daf",
+    "url": "/static/media/sweets.a2c5d884.jpg"
+  },
+  {
+    "revision": "62a607f394931d420d4f920a98a49e7f",
+    "url": "/static/media/techy.62a607f3.png"
+  },
+  {
+    "revision": "56868cd6521f405f60eb3e9835a2f215",
+    "url": "/static/media/wolf.56868cd6.svg"
+  }
+]);
