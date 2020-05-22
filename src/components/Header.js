2af7d880219a76b7309361bc6f366717e7481a1b
mgracesb <mercysbarcel@gmail.com>
2020-05-22 14:15:03 +0200

    fix errors

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from "react";
 const Header = () => {
   return (
     <header className="header">
-      <h1>Your Spirit Kawaii</h1>
+      <h1>Your Spirit Kawaii Animal</h1>
     </header>
   );
 };
